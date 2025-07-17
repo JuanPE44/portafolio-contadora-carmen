d07c31148de09198d66238bf32f5f81dce214f11
Juan Peñalba <juantandil123@gmail.com>
2025-07-17 20:44:35 -0300

    feat: main con resend agregado

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,30 @@
+import { Resend } from "resend";
+import type { APIRoute } from "astro";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+export const POST: APIRoute = async ({ request }) => {
+  try {
+    const body = await request.json();
+    const { name, email, message } = body;
+
+    const data = await resend.emails.send({
+      from: "Carmen portafolio <carmen@resend.dev>",
+      to: "estudiocontablecea@gmail.com", 
+      subject: `Nuevo mensaje de ${name}`,
+      replyTo: email,
+      html: `<p><strong>Nombre:</strong> ${name}</p>
+             <p><strong>Email:</strong> ${email}</p>
+             <p><strong>Mensaje:</strong></p>
+             <p>${message}</p>`,
+    });
+
+    return new Response(JSON.stringify({ success: true, data }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ success: false, error }), {
+      status: 500,
+    });
+  }
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,8 +3,7 @@ export const CONTACT_INFO = {
   titulo: "Oficina virtual contable",
   descripcion:
     "Estudio contable online que brinda un servicio de calidad a distancia de forma simple y cómoda. Atención personalizada sin límite de consultas.",
-  telefono: "54-2494-520102",
-  whatsapp: "54-2494-520102",
+  whatsapp: "542494521903",
   email: "estudiocontablecea@gmail.com",
   imagen: "/hero-image.png", // Ruta de la imagen de la contadora
   beneficios: [
